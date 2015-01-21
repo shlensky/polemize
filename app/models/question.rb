cb71e19bee548e4d9adadfafb9cc1eb12917609b
shlensky <shld@list.ru>
2015-01-21 23:45:16 +0300

    Add base pro & con votes count

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,6 +8,8 @@ class Question
   field :description, type: String
   field :pros_title, type: String
   field :cons_title, type: String
+  field :base_pro_votes_count, type: Integer, default: 0
+  field :base_con_votes_count, type: Integer, default: 0
   field :page_title, type: String
   field :meta_description, type: String
   field :meta_keywords, type: String
@@ -31,10 +33,10 @@ class Question
   end
 
   def pro_votes_count
-    votes.where(answer: 'pro').count
+    base_pro_votes_count + votes.where(answer: 'pro').count
   end
 
   def con_votes_count
-    votes.where(answer: 'con').count
+    base_con_votes_count + votes.where(answer: 'con').count
   end
 end
